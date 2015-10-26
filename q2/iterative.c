b280be898c6cf71d1e911f2a921c982d8a28db32
fantasticxx <phantaci00@gmail.com>
2015-10-26 17:55:03 +0800

    implement q2 implement q2 to iterative version and recusive version

--- q2/iterative.c
+++ q2/iterative.c
@@ -1,6 +1,21 @@
-/* FIXME: Implement! */
+#include<stdlib.h>
+#include<stdio.h>
+char smallest_character(char str[],char c)
+{
+    for(int i=0; i<sizeof(str)-1; i++) {
+        if(str[i]==c)
+            return str[i];
+    }
+    return str[0];
+}
+
 
 int main()
 {
+    char c,result;
+    scanf("%c",&c);
+    static char str[]= {'a','b','c','d','e'};
+    result=smallest_character(str,c);
+    printf("%c\n",result);
     return 0;
 }
--- q2/recursive.c
+++ q2/recursive.c
@@ -1,6 +1,27 @@
-/* FIXME: Implement! */
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+
+char smallest_character(char str[],int left,int right,char c)
+{
+    int mid=left+(int)((right-left)/2+0.5);
+    if(str[mid]==c) {
+        return str[mid];
+    } else if(str[mid]>c>str[0]) {
+        return smallest_character(str,left,mid-1,c);
+    } else if(str[mid]<c&&c!='\0') {
+        return smallest_character(str,mid+1,right,c);
+    } else return str[0];
+}
+
 
 int main()
 {
+    char key=NULL;
+    char result=NULL;
+    char str[5]= {'c','f','j','p','v'};
+    scanf("%c",&key);
+    result=smallest_character(str,0,5,key);
+    printf("%c\n",result);
     return 0;
 }
