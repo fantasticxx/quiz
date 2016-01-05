b53db9dd8b049b714a257737b1d6e3cf7b90b3d3
fantasticxx <phantaci00@gmail.com>
2016-01-06 00:26:23 +0800

    implemnt q27 by using card shuffle algorithm

--- q27/main.c
+++ q27/main.c
@@ -1,6 +1,28 @@
-/* FIXME: Implement! */
-
+#include<stdlib.h>
+#include<stdio.h>
+#include<time.h>
 int main()
 {
+    int card[52];
+    int s;
+    int t;
+    srand(time(NULL));
+    //generate deck
+    for(int i=0; i<52; i++) {
+        card[i]=i+1;
+    }
+    //shuffle
+    for(int j=51; j>0; j--) {
+        s=(int)(rand()%52);
+        t=card[s];
+        card[s]=card[j];
+        card[j]=t;
+    }
+
+    for(int k=0; k<52; k++) {
+        printf("%d\t",card[k]);
+    }
+
     return 0;
 }
+
